30e03c3f96e6f3b530dfda7c97332e3012d26852
Jayne Shin <shinjayne@gmail.com>
2018-11-14 08:26:39 +0900

    edit PUSH_GOODS mutations to bundle same goods
    make group of same goods in myBasket store state

--- src/store/modules/basket/mutations.js
+++ src/store/modules/basket/mutations.js
@@ -9,7 +9,24 @@ import * as types from './mutation-types';
 /* eslint-disable no-param-reassign */
 export default {
   [types.PUSH_GOODS_TO_BASKET](state, goodsAndOptions) {
-    state.myBasket.push(goodsAndOptions);
+    // find same goods
+    const index = state.myBasket.findIndex(obj => obj.goods.id === goodsAndOptions.goods.id);
+    // 같은 goods 가 없다면 새로운 goods 추가
+    if (index < 0) state.myBasket.push(goodsAndOptions);
+    else {
+      // 같은 goods 가 있다면 selectedOptions 를 조정
+      const sameGoods = state.myBasket[index];
+      goodsAndOptions.selectedOptions.map((newSelected) => {
+        const oldSelectedIndex = sameGoods.selectedOptions.findIndex(candidate => candidate.option.id === newSelected.option.id);
+        // 같은 option 없다면 새로운 selectedOption 추가
+        if (oldSelectedIndex < 0) sameGoods.selectedOptions.push(newSelected);
+        else {
+          // 같은 option 있다면  selectedOptions.count 만 조정
+          sameGoods.selectedOptions[oldSelectedIndex].count += newSelected.count;
+        }
+        return newSelected;
+      });
+    }
   },
   [types.TOGGLE_CHECK_ITEM_ON_BASKET](state, index) {
     state.myBasket[index].checked = !state.myBasket[index].checked;
