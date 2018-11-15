6366b256fc835cfafa7f24ea1c3970ca8f6e3bb8
Jayne Shin <shinjayne@gmail.com>
2018-11-15 09:52:48 +0900

    remove unused store module
    remove unused store module

--- src/store/index.js
+++ src/store/index.js
@@ -11,8 +11,6 @@ import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
 
 // Modules
-import account from './modules/account';
-import auth from './modules/auth';
 import basket from './modules/basket';
 import market from './modules/market';
 
@@ -24,8 +22,6 @@ export default new Vuex.Store({
    * Assign the modules to the store.
    */
   modules: {
-    account,
-    auth,
     basket,
     market,
   },
